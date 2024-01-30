86c6f5b5b51823a46d53492af09ef2b85eabf945
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-30 13:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let map = new Map();
+	let i=0;
+	while(i < str.length) {
+		let c = str[i];
+
+		if(map.has(c)) {
+			map.set(c,map.get(c)+1); 
+		}else{
+			map.set(c, 1);
+		}
+		i++;
+	}
+	i=0;
+	while(i<str.length) {
+		if(map.get(str[i]) == 1) return str[i];
+		i++;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
